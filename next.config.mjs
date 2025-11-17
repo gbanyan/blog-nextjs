2b573024c5fa9e84f7237269deea011414e86a31
gbanyan <gbanyan.huang@gmail.com>
2025-11-17 16:16:22 +0800

    Serve markdown assets via /assets

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,16 @@ import { withContentlayer } from 'next-contentlayer';
 const nextConfig = {
   images: {
     remotePatterns: []
+  },
+  webpack: (config) => {
+    config.ignoreWarnings = [
+      ...(config.ignoreWarnings || []),
+      // Contentlayer dynamic import / cache analysis warnings
+      /@contentlayer\/core[\\/]dist[\\/]dynamic-build\.js/,
+      /@contentlayer\/core[\\/]dist[\\/]getConfig[\\/]index\.js/,
+      /@contentlayer\/core[\\/]dist[\\/]generation[\\/]generate-dotpkg\.js/
+    ];
+    return config;
   }
 };
 
